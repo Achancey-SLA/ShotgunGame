cb727b53b06f532aa6536e67075763b1bcf4d400
alexchancey <achancey27@scienceleadership.org>
2026-05-11 14:41:00 -0400

    did some stuff

--- src/main/java/org/example/shotgungame/GameApplication.java
+++ src/main/java/org/example/shotgungame/GameApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class GameApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("lobbySelectView.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("gameView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1080, 720);
         stage.setTitle("Hello!");
         stage.setScene(scene);
--- src/main/java/org/example/shotgungame/GameController.java
+++ src/main/java/org/example/shotgungame/GameController.java
@@ -1,14 +1,53 @@
 package org.example.shotgungame;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 
 public class GameController {
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public Button startButton;
+    String name;
+    public TextField nameField;
+    InputStream inStream;
+    CommunicationConnection gameConnection;
+    ObjectInputStream myObjInput;
+    public void initialize() throws Exception {
+        // Client MUST create InputStream BEFORE OutputStream!!!!!!
     }
+
+    void messagesIn(){
+        while(true){
+            try {
+                System.out.println(myObjInput.read());
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+    public void pressStart(){
+        name = nameField.getText();
+        try {
+            Socket ourSocket = new Socket("127.0.0.1", 12345);
+            ObjectOutputStream myObjOutput = new ObjectOutputStream(ourSocket.getOutputStream());
+            myObjInput = new ObjectInputStream(ourSocket.getInputStream());
+            gameConnection = new CommunicationConnection(name, ourSocket, myObjInput, myObjOutput);
+
+            Thread communicationInThread = new Thread(this::messagesIn);
+            communicationInThread.start();
+            Message startMessage = new Message(1, "hi", name);
+            gameConnection.getOutStream().writeObject(startMessage);
+            gameConnection.getOutStream().flush();
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
 }
